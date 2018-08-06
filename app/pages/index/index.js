c167292cc2dd8dffeacc198d7e9ff41c462133bf
yezijian <yzjtjut@163.com>
2018-08-06 11:53:25 +0800

    more

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,54 +1,77 @@
-//index.js
-//获取应用实例
-const app = getApp()
+//天气对应表
+const weatherMap = {
+  'sunny' : '晴天',
+  'cloudy' : '多云',
+  'overcast' : '阴',
+  'lightrain' : '小雨',
+  'snow' : '雪'
+}
 
+//导航栏颜色对应表
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    nowTempture: '14℃',
+    nowWeather: '阴天',
+    nowWeatherBackground: '',
+    
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onPullDownRefresh(){
+    this.getNow(()=>{
+      wx.stopPullDownRefresh()
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  onLoad(){
+    this.getNow()
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+  //页面信息
+  getNow(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
+
+
+      data: {
+        city: '深圳市',
+      },
+
+
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+
+
+      success: res => {
+        console.log(res.data)
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        console.log(temp, weather)
+        this.setData({
+          nowTempture: temp+'℃',
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground : '../../img/'+weather+'-bg.png'
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      },
+
+      //结束刷新
+      complete: ()=>{
+          callback && callback()
+      }
+      
+
+
     })
   }
+ 
 })
